c3794976034060cf55d71fe037213a10b4bd9233
Marc CRAMPE <107358477+Marccrampe@users.noreply.github.com>
2025-06-25 15:42:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -387,21 +387,20 @@ with tabs[1]:
 
 #----  tab 3 -------------
 
-with tabs[2]:
-
-st.markdown("### 📘 OpenAtlas User Guide – Full Viewer")
-import streamlit as st
-import base64
-
-pdf_path = "Geojson_guide.pdf"
-
-with open(pdf_path, "rb") as f:
-    base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px" type="application/pdf"></iframe>'
-st.markdown(pdf_display, unsafe_allow_html=True)
-
-)
+    with tabs[2]:
+    
+    st.markdown("### 📘 OpenAtlas User Guide – Full Viewer")
+    import streamlit as st
+    import base64
+    
+    pdf_path = "Geojson_guide.pdf"
+    
+    with open(pdf_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800px" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+    
 
 
     # Download button
